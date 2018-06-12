95c592626d7eb8550d12c5903da87d8a74950b7c
mpais110 <33531303+mpais110@users.noreply.github.com>
2018-06-12 01:03:08 -0300

    31

--- ObligatorioAYED2/src/TADLista/Lista.java
+++ ObligatorioAYED2/src/TADLista/Lista.java
@@ -1,41 +1,96 @@
 package TADLista;
 
-import java.util.Iterator;
 
-public class Lista<T> implements Iterable<T> 
-{
-    protected NodoLista<T> inicio;
+public class Lista<T> {
+    private NodoLista<T> inicio;
+    int tamano;
+    
 
-    public Lista() {
-	this.inicio = null;
+    //Constructor
+    public Lista()
+    {
+        inicio = null;
+        tamano = 0;
+    }
+    
+    
+    public NodoLista<T> getInicio() {
+        return inicio;
     }
 
+    
     public void insertar(T dato)
     {
-            NodoLista<T> nuevo = new NodoLista<T>(dato, inicio);
-            inicio = nuevo;
+        NodoLista<T> nuevo = new NodoLista<T>(dato, inicio);
+        inicio = nuevo;
     }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-
-            NodoLista<T> aux = inicio;
-
-            @Override
-            public boolean hasNext() {
-                    return aux != null;
-            }
-
-            @Override
-            public T next() {
-                    T ret = aux.getDato();
-                    aux = aux.getSig();
-                    return ret;
-            }
-
-        };
+    
+    
+//    public void insertaInicioChofer(Object obj)
+//    {
+//        if (inicio == null)
+//        {
+//            inicio = new NodoLista(obj);
+//        }
+//        else
+//        {
+//            NodoLista aux = inicio;
+//            NodoLista nuevo = new NodoLista(obj);
+//            nuevo.setSig(aux);
+//            inicio = nuevo;
+//        }
+//        tamano++;
+//    }
+    
+    
+   
+    public int tamano() 
+    {
+        return tamano;
     }
+    
+    
+ 
+    
+    public boolean estaVacia()
+    {
+        return (inicio == null);
+    }
+    
+    
+    public void eliminarPrimero()
+    {
+        inicio = inicio.getSig();
+        tamano--;
+    }
+    
 
-	
+    public void vaciarLista() {
+        this.inicio = null;
+        this.tamano = 0;
+    }
+    
+    
+    
+    public void eliminarEnIndice(int ind)
+    {
+        if (ind == 0)
+        {
+            inicio = inicio.getSig();
+        }
+        else
+        {
+            int contador = 0;
+            NodoLista aux = inicio;
+            while(contador < ind - 1)
+            {
+                aux = aux.getSig();
+                contador++;
+            }
+            aux.setSig(aux.getSig().getSig());
+        }
+        tamano--;
+    }
 }
+
+
